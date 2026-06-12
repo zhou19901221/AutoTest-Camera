296a5004a03b49adcaea3c442c129a3287f1a5d0
hw012734829 <hw012734829@example.com>
2026-06-12 18:33:55 +0800

    为系统设置页面创建UserControl架构，基础参数和检测设置已可在设计器中编辑

---
+++ MESS设置控件.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+namespace 自动测试
+{
+    public partial class MESS设置控件 : UserControl
+    {
+        public MESS设置控件()
+        {
+            InitializeComponent();
+        }
+    }
+}
